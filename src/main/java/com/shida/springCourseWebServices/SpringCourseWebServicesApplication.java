410a378ffb39ca0ce7be4d7061c6aeb9fb99af83
Shida <marcellinhonkai@gmail.com>
2024-12-15 00:35:05 -0300

    Project created

---
+++ src/main/java/com/shida/springCourseWebServices/SpringCourseWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.shida.springCourseWebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shida/springCourseWebServices/SpringCourseWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shida.springCourseWebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
