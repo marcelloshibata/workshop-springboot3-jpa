21c065a0216921de8358f22d44a574f214133142
Shida <marcellinhonkai@gmail.com>
2024-12-16 16:07:30 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/shida/springCourseWebServices/config/TestConfig.java
@@ -0,0 +1,2 @@
+package com.shida.springCourseWebServices.config;public class TestConfig {
+}
---
+++ src/main/java/com/shida/springCourseWebServices/repositories/UserRepository.java
@@ -0,0 +1,4 @@
+package com.shida.springCourseWebServices.repositories;
+
+public class UserRepository {
+}
