f593e5ec5825afa8039bc0a3aa46808d8d219f16
Shida <marcellinhonkai@gmail.com>
2025-01-02 15:04:34 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/shida/springCourseWebServices/config/TestConfig.java
+++ src/main/java/com/shida/springCourseWebServices/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.shida.springCourseWebServices.config;
 
-import com.shida.springCourseWebServices.entities.Category;
-import com.shida.springCourseWebServices.entities.Order;
-import com.shida.springCourseWebServices.entities.Product;
-import com.shida.springCourseWebServices.entities.User;
+import com.shida.springCourseWebServices.entities.*;
 import com.shida.springCourseWebServices.entities.enums.OrderStatus;
-import com.shida.springCourseWebServices.repositories.CategoryRepository;
-import com.shida.springCourseWebServices.repositories.OrderRepository;
-import com.shida.springCourseWebServices.repositories.ProductRepository;
-import com.shida.springCourseWebServices.repositories.UserRepository;
+import com.shida.springCourseWebServices.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -69,5 +66,12 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
